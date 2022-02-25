996ee28c6e539671b9bf0a5ba6edb009095dd55b
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-02-24 17:12:45 -0800

    loading animation

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// show loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// hide loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // get quotes from API
 async function getQuotes() {
+  loading();
   const apiUrl = 'https://icanhazdadjoke.com/';
   try {
     var myHeaders = new Headers();
@@ -32,14 +46,27 @@ async function getQuotes() {
 
 // show new quote
 function newQuote() {
+  loading();
   // check quote length to determine styling
   if (apiQuotes.length > 60) {
     quoteText.classList.add('long-quote');
   } else {
     quoteText.classList.remove('long-quote');
   }
+  // set quote, hide loader
   quoteText.textContent = apiQuotes;
+  complete();
 }
+
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent}&hashtags=dadjokes,dadquotes`;
+  window.open(twitterUrl, '_blank');
+}
+
+// event listeners
+newQuoteBtn.addEventListener('click', getQuotes);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // on load
 getQuotes();
