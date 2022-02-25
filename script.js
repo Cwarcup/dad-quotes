b4afb5340f3c2ae74d855d3f75fcf10a451737ad
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-02-24 16:47:08 -0800

    DOM quote added

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 // get quotes from API
@@ -15,12 +21,24 @@ async function getQuotes() {
 
     const response = await fetch(apiUrl, requestOptions)
       .then((response) => response.json())
-      .then((result) => (apiQuotes = result))
+      .then((result) => (apiQuotes = result.joke))
       .catch((error) => console.log('error', error));
     console.log(apiQuotes);
+    newQuote();
   } catch (error) {
     console.log('ERROR! ', error);
   }
+}
+
+// show new quote
+function newQuote() {
+  // check quote length to determine styling
+  if (apiQuotes.length > 60) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+  quoteText.textContent = apiQuotes;
 }
 
 // on load
